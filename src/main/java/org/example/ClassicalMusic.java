4a0fe0484ea6772beefb5b8ceddf35870a4966f0
oger_si <stas.oger.99@mail.ru>
2022-04-18 17:23:56 +0300

    (Bean Lifecycle). Init, Destroy и Factory методы

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class ClassicalMusic implements Music {
 
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destraction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,23 +8,26 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean compasion = firstMusicPlayer == secondMusicPlayer;
-
-        System.out.println(compasion);
-
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(10);
-
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean compasion = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(compasion);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
