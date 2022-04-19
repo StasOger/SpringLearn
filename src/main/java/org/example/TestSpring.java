f6f06bf137fa0d724ccf29d980027ae54fbb4889
oger_si <stas.oger.99@mail.ru>
2022-04-19 16:03:11 +0300

    @Autowired. Внедрение зависимостей (Dependency Injection).

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,37 +8,23 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-//        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-//        System.out.println(classicalMusic.getSong());
-
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
-        musicPlayer.playMusic();
-
-//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        Music music = context.getBean("rockMusic", Music.class);
 //
-//        boolean compasion = firstMusicPlayer == secondMusicPlayer;
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 //
-//        System.out.println(compasion);
+//        musicPlayer.playMusic();
 //
-//        System.out.println(firstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
+//        Music music2 = context.getBean("classicalMusic", Music.class);
 //
-//        firstMusicPlayer.setVolume(10);
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
 //
-//        System.out.println(firstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getVolume());
+//        classicalMusicPlayer.playMusic();
 
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-        Music music2 = context.getBean("classicalMusic", Music.class);
-
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-
-        classicalMusicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
